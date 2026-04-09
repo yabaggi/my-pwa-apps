0f233c173dea15daf05a0a67d3e5a939e0f73591
yabaggi <yabaggi@gmail.com>
2026-04-09 10:57:43 +0400

    push notif added

---
+++ prom/firebase-massaging-sw.js
@@ -0,0 +1,9 @@
+importScripts('https://gstatic.com');
+importScripts('https://gstatic.com');
+
+firebase.initializeApp({
+  messagingSenderId: "903495800021" 
+});
+
+const messaging = firebase.messaging();
+
